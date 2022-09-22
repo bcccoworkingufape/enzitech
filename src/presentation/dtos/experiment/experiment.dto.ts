c7dc39eba1a77bfff5b294ec1915c6a8bb99e338
Matheus Noronha <noronhamatheus07@gmail.com>
2022-09-21 22:02:31 -0300

    feat: experiment dtos

---
+++ src/presentation/dtos/experiment/experiment.dto.ts
@@ -0,0 +1,40 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { Expose, plainToClass } from 'class-transformer';
+import { IsNumber, IsOptional, IsString } from 'class-validator';
+import { ExperimentEnzyme } from '@/domain/models/experiment-enzyme.entity';
+import { ProcessDto } from '../process/process.dto';
+import { BaseExperimentDto } from './base-experiment.dto';
+import { ExperimentEnzymeDto } from './experiment-enzyme.dto';
+import { Process } from '@/domain/models/process.entity';
+
+export class ExperimentDto extends BaseExperimentDto {
+  @ApiProperty({
+    type: [ProcessDto],
+    description: 'processes',
+    required: true,
+  })
+  @Expose()
+  readonly processes: ProcessDto[];
+
+  @ApiProperty({
+    type: [ExperimentEnzymeDto],
+    description: 'enzymes',
+    required: true,
+  })
+  @Expose()
+  readonly enzymes: ExperimentEnzymeDto[];
+
+
+  constructor(obj: ExperimentDto, experimentEnzymes: ExperimentEnzyme[], processes: Process[]) {
+    super(obj);
+
+    this.enzymes = experimentEnzymes.map(experimentEnzyme => new ExperimentEnzymeDto({
+      ...experimentEnzyme,
+      name: experimentEnzyme.enzyme.name,
+      type: experimentEnzyme.enzyme.type,
+      id: experimentEnzyme. enzyme.id
+
+    }));
+    this.processes = processes.map(process => new ProcessDto({...process}));
+  }
+}
